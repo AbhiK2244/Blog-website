5057d836cbb5e78c89aabbe3429372c91de3f2f3
Abhishek Kamati <kamtiabhi4@gmail.com>
2024-10-03 10:02:23 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -13,11 +13,11 @@ const port = 3000;
 
 let connection = await mongoose.connect(process.env.DB_CONNECTION_STRING);
 
-// app.use(cors({
-//     origin: "https://blog-website-frontend-azure.vercel.app",
-//     methods: ["POST", "GET", "DELETE", "PUT"],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: "https://blog-website-frontend-azure.vercel.app",
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    // credentials: true
+}))
 
 app.use(bodyParser.json())
 
